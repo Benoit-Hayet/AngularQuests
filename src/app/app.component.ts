b91423d4f26897824143bf3d309cae7b0330cf92
Benoit-Hayet <hayetbenoit@gmail.com>
2024-02-29 16:11:54 +0100

    directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { BlockComponent } from './block/block.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { FormsModule } from '@angular/forms';
+import { DirectivesComponent } from './directives/directives.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,BlockComponent,UserProfileComponent,FormsModule],
+  imports: [RouterOutlet,BlockComponent,UserProfileComponent,FormsModule,DirectivesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
