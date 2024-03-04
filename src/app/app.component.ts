c35dfde53e5ed7a2628c84db49434c50a8ee895f
Benoit-Hayet <hayetbenoit@gmail.com>
2024-03-04 15:34:05 +0100

    Routes Ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink} from '@angular/router';
 import { BlockComponent } from './block/block.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { FormsModule } from '@angular/forms';
 import { DirectivesComponent } from './directives/directives.component';
+import { MenuComponent } from './menu/menu.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,BlockComponent,UserProfileComponent,FormsModule,DirectivesComponent],
+  imports: [RouterOutlet,RouterLink,BlockComponent,UserProfileComponent,FormsModule,DirectivesComponent,MenuComponent,SignUpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
